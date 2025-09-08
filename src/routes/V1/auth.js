c54ccb56b8be016184ad86e2c6c603c110e3bf28
nikhileshwar-perumalla <nikhileshwarperumalla@gmail.com>
2025-09-08 14:54:14 +0530

    added email and password validations

--- src/routes/V1/auth.js
+++ src/routes/V1/auth.js
@@ -1,9 +1,31 @@
 import { Router } from "express";
 import register from '../../controllers/V1/Auth/register.js';
+import { body } from "express-validator";
+import validationError from '../../../middlewares/validationError.js';
+import  User from '../../../models/user.js'
 
 
 const router = Router();
 
-router.post('/register', register);
+router.post('/register',body('email')
+        .trim()
+        .notEmpty()
+        .withMessage('Email is required')
+        .isLength({max : 40})
+        .withMessage('Email must be less than 40')
+        .isEmail()
+        .withMessage('Please enter a valid email')
+        .custom(async(value) => {
+            const userExists = await User.exists({ email: value });
+            if (userExists) {
+                throw new Error('Email is already in use');
+            }
+        }),
+    body('password').trim().notEmpty().withMessage('password must be there')
+    .isLength({ max : 30 ,min : 8}).withMessage('Password must be less than 30 and more than 8 characters'),
+    body('role').optional().notEmpty().withMessage('Role must be there')
+    .isString().withMessage('role must be only string').isIn(['user','admin']).withMessage('role must be a user or admin'),
+    validationError
+    , register)
 
 export default router;
